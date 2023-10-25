6d18496a96e65eb5e5c2d59ebb639786234a8897
AmrSaleh2234 <amrsaleh1001@gmail.com>
2023-10-25 11:53:31 +0300

    .

--- laravel/Modules/Evaluation/Http/Controllers/Repository/EavlautionRepository.php
+++ laravel/Modules/Evaluation/Http/Controllers/Repository/EavlautionRepository.php
@@ -90,29 +90,29 @@ class EavlautionRepository
         if ($date2 == null || $date1 == null)
             return EavlautionRepository::getResultForSpecificChildWithSpecificSideProfile($child, $sideProfile, $evaluation);
 
-        if ($date1 > $date2) {
-            $temp = $date2;
-            $date2 = $date1;
-            $date1 = $temp;
-        }
-        return EvaluationResults::selectRaw("children.name child_name ,
-         users.name therapist_name ,
-        evaluations.title evaluation_title ,
-        evaluation_results.grow_age grow_age ,
-         evaluation_results.late_percentage late_percentage ,
-         evaluation_results.diff_age diff_age ,
-          evaluation_results.id id ,
-          evaluation_results.basal_age basal_age ,
-           evaluation_results.created_at result_created_at")
-            ->join("evaluations", "evaluation_results.evaluation_id", '=', 'evaluations.id')
-            ->join("children", "evaluation_results.child_id", '=', 'children.id')
-            ->join("users", "evaluation_results.therapist_id", '=', 'users.id')
-            ->where('child_id', $child->id)
-            ->where('evaluations.side_profile_id', $sideProfile->id)
-            ->where('evaluations.id', $evaluation->id)
-            ->where('evaluation_results.created_at', '>=', $date1)
-            ->where('evaluation_results.created_at', '<=', $date2)
-            ->get();
+//        if ($date1 > $date2) {
+//            $temp = $date2;
+//            $date2 = $date1;
+//            $date1 = $temp;
+//        }
+//        return EvaluationResults::selectRaw("children.name child_name ,
+//         users.name therapist_name ,
+//        evaluations.title evaluation_title ,
+//        evaluation_results.grow_age grow_age ,
+//         evaluation_results.late_percentage late_percentage ,
+//         evaluation_results.diff_age diff_age ,
+//          evaluation_results.id id ,
+//          evaluation_results.basal_age basal_age ,
+//           evaluation_results.created_at result_created_at")
+//            ->join("evaluations", "evaluation_results.evaluation_id", '=', 'evaluations.id')
+//            ->join("children", "evaluation_results.child_id", '=', 'children.id')
+//            ->join("users", "evaluation_results.therapist_id", '=', 'users.id')
+//            ->where('child_id', $child->id)
+//            ->where('evaluations.side_profile_id', $sideProfile->id)
+//            ->where('evaluations.id', $evaluation->id)
+//            ->where('evaluation_results.created_at', '>=', $date1)
+//            ->where('evaluation_results.created_at', '<=', $date2)
+//            ->get();
     }
 
     public static function getEvaluationsForSpecificChildWithSpecificSideProfile($child, $sideProfile)
